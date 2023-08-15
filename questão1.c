e128ca78e866bad34080e0c8a2896d8837b65179
Roberto_F.rocha <115853601+Roberto-F-Rocha@users.noreply.github.com>
2023-08-15 11:56:16 -0300

    Update questão1.c

--- questão1.c
+++ questão1.c
@@ -1,21 +1,30 @@
-//Escreva um programa para converter e imprimir os caracteres para os valores ASCII de 0 a 127.
-//O programa deve imprimir 10 caracteres por linha.
-
 #include <stdio.h>
 
-int main(){
+/*
+1. Escreva um programa para converter e imprimir os caracteres para os valores ASCII de 0 a 127.
+O programa deve imprimir 10 caracteres por linha.
+*/
+
+int main() {
     char c; 
     int i, count = 0;
-    
-    for(i = 0; i < 127; i++){
-    	c = i;   
-    	printf(" %c", c);
-    	count++; //após cada caractere ser impresso, essa linha é executada incrementando +1.
-    	if(count == 10){
-        	printf("\n");
-        	count = 0; //reseta o valor de 'count' para 0, para começar a contar novamente.
-    	}
+
+    for (i = 0; i < 127; i++) {
+      // Inicia um loop que vai de 0 até 126.
+        c = i;   
+        // Atribui o valor de 'i' à variável 'c'.
+        printf(" %c", c);
+        // foi definido como 'c' para imprimir como caractere./ 
+        count++;
+        // Incrementa o contador 'count' por 1.
+        if (count == 10) {
+            // Verifica se já foram impressos 10 caracteres.
+            printf("\n");
+            // Imprime uma quebra de linha para começar uma nova linha.
+            count = 0;
+            // Reinicia o contador 'count' para 0.
+        }
     }
     
- return(0);
+    return(0);
 }
